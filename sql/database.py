f2e6ab603ad9c77e4ea8354032e20d2b144863b6
Felipe Urrutia <thewomins@gmail.com>
2022-11-10 17:36:33 -0300

    Update env for heroku

--- sql/database.py
+++ sql/database.py
@@ -1,15 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import dotenv_values
-
-values = dotenv_values(".env")
+import os
 
 SQLALCHEMY_DATABASE_URL = "mysql+pymysql://{user}:{password}@{host}:3306/{db}".format(
-    user=values.get("MYSQL_USER"),
-    password=values.get("MYSQL_PASS"),
-    host=values.get("MYSQL_HOST"),
-    db=values.get("MYSQL_NAME"),
+    user=os.environ.get("MYSQL_USER"),
+    password=os.environ.get("MYSQL_PASS"),
+    host=os.environ.get("MYSQL_HOST"),
+    db=os.environ.get("MYSQL_NAME"),
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_timeout=5, pool_recycle=4)
